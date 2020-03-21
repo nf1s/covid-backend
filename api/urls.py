78b7e78f8934b2c336e684150e7647d9fa1f7997
ahmednafies <ahmed.nafies@gmail.com>
2020-03-21 23:38:31 +0100

    fixes urls

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,8 @@ from sanic import Blueprint
 from sanic_transmute import add_route
 from .views import (
     get_all,
-    get_status_by_id,
-    get_status_by_name,
+    get_status_by_country_id,
+    get_status_by_country_name,
     get_deaths,
     get_active_cases,
     get_recovered_cases,
@@ -13,8 +13,8 @@ from .views import (
 
 cases = Blueprint("cases", url_prefix="/cases")
 add_route(cases, get_all)
-add_route(cases, get_status_by_id)
-add_route(cases, get_status_by_name)
+add_route(cases, get_status_by_country_id)
+add_route(cases, get_status_by_country_name)
 add_route(cases, get_deaths)
 add_route(cases, get_active_cases)
 add_route(cases, get_recovered_cases)
